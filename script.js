57017535756beb1459d795bc71af75846ffafa50
Amarachii <amarachijacinta2019@gmail>
2023-11-17 23:08:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) =>{
+    event.target.style.background = "lightcoral";
+    event.target.style.color= "white";
+});
+
+const change2 = document.getElementById("image2");
+
+change2.addEventListener("click", (event) =>{
+    event.target.style.background = "rgb(95, 95, 255)";
+    event.target.style.color = "white";
+});
+
+const change3 = document.getElementById("image3");
+
+change3.addEventListener("click", (event) =>{
+    event.target.style.background = "bisque";
+    event.target.style.color = "black";
+});
+
+const change4 = document.getElementById("image4");
+
+change4.addEventListener("click", (event) =>{
+    event.target.style.background = "rgb(200, 162, 200)";
+    event.target.style.color = "black";
+});
+
+const change5 = document.getElementById("image5");
+
+change5.addEventListener("click", (event) =>{
+    event.target.style.background = "rgb(205, 91, 69)";
+    event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("image6");
+
+change6.addEventListener("click", (event) =>{
+    event.target.style.background = "rgb(74, 44, 42)";
+    event.target.style.color = "white";
+});
